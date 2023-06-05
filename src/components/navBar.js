a67ce66ffef71a892a8c2e0942c2861831f500b3
Arief Budiman <mariefbudiman12@gmail.com>
2023-06-05 11:48:49 +0700

    menambah fitur navbar

---
+++ src/components/navBar.js
@@ -0,0 +1,27 @@
+import MyPict from "../assets/profile/myPict.png";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <div className="logo">
+          <a href="/"><p>CINEMA</p></a>
+        </div>
+        <div className="menu">
+          <ul>
+            <li><a href="#">GENRE</a></li>
+            <li><a href="#">POPULAR</a></li>
+          </ul>
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search..." id="search_input" />
+        </div>
+        <div className="img_profile">
+          <a href="#"><img src={MyPict} alt="" /></a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
