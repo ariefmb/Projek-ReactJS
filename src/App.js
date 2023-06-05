670f9f38eb201024a54e4d74b0b376cdfa144430
Arief Budiman <mariefbudiman12@gmail.com>
2023-06-05 23:37:58 +0700

    menambah tulisan Popular

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
 import "./style/App.css";
-import {getMovieList, searchMovie} from './api'
+import { getMovieList, searchMovie } from "./api";
 import { useEffect, useState } from "react";
-import NavBar from "./components/navBar"
+import NavBar from "./components/navBar";
 
 const App = () => {
-  const [popularMovies, setPopularMovies] = useState([])
+  const [popularMovies, setPopularMovies] = useState([]);
 
   useEffect(() => {
     getMovieList().then((result) => {
-      setPopularMovies(result)
-    })
-  }, [])
+      setPopularMovies(result);
+    });
+  }, []);
 
   const PopularMovieList = () => {
     return popularMovies.map((movie, i) => {
       return (
         <div className="movie-wrapper" key={i}>
           <div className="movie-title">{movie.title}</div>
-          <img 
-          className="movie-img" 
-          src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} 
+          <img
+            className="movie-img"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
           />
           <div className="movie-date">release: {movie.release_date}</div>
           <div className="movie-rate">{movie.vote_average}</div>
         </div>
-      )
-    })
-  }
+      );
+    });
+  };
 
-  const search = async(q) => {
-    if(q.length > 3) {
-      const query = await searchMovie(q)
-      setPopularMovies(query.results)
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
     }
-  }
+  };
 
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         {/* NavBar */}
         <div className="nav">
           <NavBar />
@@ -47,16 +47,20 @@ const App = () => {
         className="movie-search"
         onChange={({target}) => search(target.value)} 
         /> */}
+        <div className="type">
+          <h1>POPULAR</h1>
+          <h3>see all</h3>
+        </div>
         <div className="movie-container">
           <PopularMovieList />
         </div>
       </header>
-      
-      <footer className='App-footer'>
+
+      <footer className="App-footer">
         &copy;2023. <b>Arief Budiman</b>
       </footer>
     </div>
-  )
-}
+  );
+};
 
 export default App;
