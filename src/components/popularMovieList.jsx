dfbc49deaf6a40bd9c4be60cb5fda8a896a4d778
Arief Budiman <mariefbudiman12@gmail.com>
2023-06-19 15:45:25 +0700

    desain gambar menjadi bg

--- src/components/popularMovieList.jsx
+++ src/components/popularMovieList.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { getMovieList } from "../api";
 
 const PopularMovieList = ({ searchValue }) => {
-  const [searchValue, setPopularMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
 
   useEffect(() => {
     getMovieList().then((result) => {
@@ -11,16 +11,19 @@ const PopularMovieList = ({ searchValue }) => {
   }, [searchValue]);
 
   const ListPopularMovie = () => {
-    return searchValue.map((movie, i) => {
+    return popularMovies.map((movie, i) => {
       return (
-        <div className="movie-wrapper" key={i}>
-          <div className="movie-title">{movie.title}</div>
-          <img
-            className="movie-img"
-            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
-          />
-          <div className="movie-date">release: {movie.release_date}</div>
-          <div className="movie-rate">{movie.vote_average}</div>
+        <div
+          className="movie-wrapper"
+          key={i}
+          style={{
+            backgroundImage: `url(${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path})`,
+          }}>
+          <div className="movie-content">
+            <div className="movie-title">{movie.title}</div>
+            <div className="movie-date">release: {movie.release_date}</div>
+            <div className="movie-rate">{movie.vote_average}</div>
+          </div>
         </div>
       );
     });
