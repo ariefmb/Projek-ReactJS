332d0ba4f7471ac7fd410b207f510a04f3ea7305
Arief Budiman <mariefbudiman12@gmail.com>
2023-06-23 21:31:06 +0700

    desain profile page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../style/profile.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
 import MyPict from "../assets/profile/myPict.png";
 import instagram from "../assets/sosmed/instagram.png";
 import whatsapp from "../assets/sosmed/whatsapp.png";
 import github from "../assets/sosmed/github.png";
 
 export default function ProfilePage() {
+
   return (
     <div className="Profile">
       <header className="Profile-header">
         <nav className="Nav-profile">
           <Link to="/">
-            <button>Back to Main</button>
+            <FontAwesomeIcon icon={faArrowLeft} className="arrow" />
           </Link>
           <h1>PROFILE PAGE</h1>
         </nav>
 
         <div className="Profile-content">
           <div className="left-content">
-            <div className="img_profile">
+            <div className="img-profile">
               <img src={MyPict} alt="profile" />
               <h1>ARIEFMB</h1>
             </div>
@@ -50,23 +54,43 @@ export default function ProfilePage() {
             <div className="bio">
               <div className="bio-card">
                 <h2>First Name</h2>
-                <input type="text" placeholder="First Name" />
+                <input type="text" value="Muhammad" readOnly />
               </div>
               <div className="bio-card">
                 <h2>Mid Name</h2>
-                <input type="text" placeholder="Mid Name" />
+                <input type="text" value="Arief" readOnly />
               </div>
               <div className="bio-card">
                 <h2>Last Name</h2>
-                <input type="text" placeholder="Last Name" />
+                <input type="text" value="Budiman" readOnly />
+              </div>
+              <div className="bio-card">
+                <h2>Born</h2>
+                <input type="date" id="born" />
+              </div>
+              <div className="bio-card">
+                <h2>Description</h2>
+                <textarea
+                  rows="5"
+                  cols="83"
+                  placeholder="Description..."
+                ></textarea>
               </div>
             </div>
+            <div className="submit">
+              <button id="submit-btn">SUBMIT</button>
+              <button id="cancel-btn">CANCEL</button>
+            </div>
           </div>
+        </div>
+
+        <div className="logout">
+          <button id="logout-btn">LOGOUT</button>
         </div>
       </header>
 
       <footer className="Profile-footer">
-        &copy;2023. <b>Arief Budiman</b>
+        &copy;2023. <b> Arief Budiman</b>
       </footer>
     </div>
   );
